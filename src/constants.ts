220b7dd6abb71d76f8995c27ea40d04959fcca4c
Jeremy Dombrowski <dev@jeremyfelix.com>
2020-11-09 18:09:55 -0800

    Add ES6 back quote support

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,12 @@ export const SELECT_QUOTES_COMMAND_ID = "select-quotes";
 
 export const DOUBLE_QUOTE = "\"";
 export const SINGLE_QUOTE = "'";
+export const BACK_QUOTE = "`";
 
 export const DEFAULT_POSITION:  Position  = new Position(0,0);
 export const DEFAULT_SELECTION: Selection = new Selection(DEFAULT_POSITION, DEFAULT_POSITION);
 
-export const isQuote = (c: string) => c === DOUBLE_QUOTE || c === SINGLE_QUOTE;
+export const isQuote = (c: string) => c === DOUBLE_QUOTE || c === SINGLE_QUOTE || c === BACK_QUOTE;
 
 export const isMouseEvent             = (e: TextEditorSelectionChangeEvent) => e.kind === TextEditorSelectionChangeKind.Mouse;
 export const isOnlyOneActiveSelection = (e: TextEditorSelectionChangeEvent) => e.selections.length === 1;
